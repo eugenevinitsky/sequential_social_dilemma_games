7784ef9e377b5373c061487973bf42ea0e301866
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-05 20:56:02 -0800

    started

---
+++ renderer.py
@@ -0,0 +1,6 @@
+# quick build based off of
+# https://github.com/fogleman/Minecraft/blob/master/main.py
+
+from pyglet import image
+from pyglet.gl import *
+from pyglet.graphics import TextureGroup
