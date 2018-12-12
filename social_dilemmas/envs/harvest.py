c5ae7a43c38b2f6e131545f65b29187d4601de97
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-12 12:50:39 -0800

    spawn apples method

--- social_dilemmas/envs/harvest.py
+++ social_dilemmas/envs/harvest.py
@@ -6,26 +6,26 @@ from social_dilemmas.envs.agent import HarvestAgent
 from social_dilemmas.constants import HARVEST_MAP
 from social_dilemmas.envs.map_env import MapEnv
 
-
 APPLE_RADIUS = 2
 
-COLOURS = {' ': [0, 0, 0],        # Black background
-           '@': [764, 0, 999],    # Board walls
-           'A': [0, 999, 0],      # Green apples
-           'P': [0, 999, 999],    # Player #FIXME(ev) agents need to have different colors
-           'F': [999, 999, 0]}    # Yellow firing beam
+COLOURS = {' ': [0, 0, 0],  # Black background
+           '@': [764, 0, 999],  # Board walls
+           'A': [0, 999, 0],  # Green apples
+           'P': [0, 999, 999],  # Player #FIXME(ev) agents need to have different colors
+           'F': [999, 999, 0]}  # Yellow firing beam
 
 # use keyword names so that it's easy to understand what the agent is calling
-ACTIONS = {'MOVE_LEFT':             [-1, 0],  # Move left
-           'MOVE_RIGHT':            [1, 0],   # Move right
-           'MOVE_UP':               [0, 1],   # Move up
-           'MOVE_DOWN':             [0, -1],  # Move down
-           'STAY':                  [0, 0],   # don't move
-           'TURN_CLOCKWISE':        [[0, 1], [-1, 0]],  # Rotate counter clockwise
-           'TURN_COUNTERCLOCKWISE': [[0, -1], [1, 0]],   # Move right
-           'FIRE': 5}               # Fire 5 squares forward #FIXME(ev) is the firing in a straight line?
+ACTIONS = {'MOVE_LEFT': [-1, 0],  # Move left
+           'MOVE_RIGHT': [1, 0],  # Move right
+           'MOVE_UP': [0, 1],  # Move up
+           'MOVE_DOWN': [0, -1],  # Move down
+           'STAY': [0, 0],  # don't move
+           'TURN_CLOCKWISE': [[0, 1], [-1, 0]],  # Rotate counter clockwise
+           'TURN_COUNTERCLOCKWISE': [[0, -1], [1, 0]],  # Move right
+           'FIRE': 5}  # Fire 5 squares forward #FIXME(ev) is the firing in a straight line?
 
-spawn_prob = {1: }
+SPAWN_PROB = [0, 0.005, 0.02, 0.05]
+
 
 class HarvestEnv(MapEnv):
 
@@ -52,7 +52,6 @@ class HarvestEnv(MapEnv):
         for agent in self.agents.values():
             new_pos = self.spawn_point()
             agent.set_pos(new_pos)
-
 
     def update_map(self, agent_actions):
         """Converts agent action tuples into a new map and new agent positions
@@ -98,10 +97,26 @@ class HarvestEnv(MapEnv):
         # first pad the matrix so that we can iterate through nicely
         # FIXME(ev) magic number
         l2_dist = 2
-        pad_mat = self.pad_matrix(2, 2, 2, 2, self.base_map, ' ')
+        pad_mat = self.pad_matrix(2, 2, 2, 2, self.mat, ' ')
+        new_map = np.zeros(self.mat.shape)
         for row in enumerate(self.base_map.shape[0]):
             for col in enumerate(self.base_map.shape[1]):
-                pass
+                # if we have a spawn point, scan around in the actual map
+                if self.base_map[row, col] == 'A':
+                    # look in a l2 window
+                    window = pad_mat[row - l2_dist + 1:row + l2_dist,
+                             col - l2_dist + 1:col + l2_dist]
+                    # compute how many apples are in window
+                    unique, counts = np.unique(window, return_counts=True)
+                    counts_dict = dict(zip(unique, counts))
+                    num_apples = counts_dict['A']
+                    spawn_prob = SPAWN_PROB[min(num_apples, 3)]
+                    rand_num = np.random.rand(1)[0]
+                    if rand_num < spawn_prob:
+                        new_map[row, col] = 'A'
+                    # FIXME(should try to add this all in one pass)
+                    # FIXME(ev) can an apple spawn where a player is standing?
+        return new_map
 
     def spawn_point(self):
         """Returns a randomly selected spawn point"""
