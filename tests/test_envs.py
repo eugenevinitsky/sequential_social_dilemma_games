75846b1a0db84b55350dcf07cdb38162c8b7e764
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-14 11:44:24 -0800

    git push origin unit_tests

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,5 +1,7 @@
 '''Unit tests for all of the envs'''
 
+import numpy as np
+
 from social_dilemmas.envs.harvest import HarvestEnv
 
 MINI_HARVEST_MAP = [
@@ -27,6 +29,15 @@ class TestHarvestEnv(unittest.TestCase):
     def test_reset(self):
         self.env.reset()
 
+    def test_walls(self):
+        """Check that the spawned map and base map have walls in the right place"""
+        self.env.reset()
+        np.testing.assert_array_equal(self.env.base_map[0,:], np.array(['@']*6))
+        np.testing.assert_array_equal(self.env.base_map[-1, :], np.array(['@'] * 6))
+        np.testing.assert_array_equal(self.env.base_map[:, 0], np.array(['@'] * 6))
+        np.testing.assert_array_equal(self.env.base_map[:, -1], np.array(['@'] * 6))
+
+
     def test_view(self):
         """Confirm that an agent placed at the right point returns the right view"""
         pass
@@ -38,6 +49,9 @@ class TestHarvestEnv(unittest.TestCase):
         pass
 
     def test_move_agent(self):
+        # if an agent tries to move through a wall they should stay in the same place
+
+        # if an agent moves over an apple the apple disappears
         pass
 
     def test_agent_rewards(self):
