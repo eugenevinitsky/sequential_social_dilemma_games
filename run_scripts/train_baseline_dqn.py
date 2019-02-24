70b1297f682b95bffc2d61f60d7aa192b68e5762
eugenevinitsky <vinitsky.eugene@gmail.com>
2019-02-24 09:44:25 -0800

    Add DQN run script

---
+++ run_scripts/train_baseline_dqn.py
@@ -0,0 +1,154 @@
+import ray
+from ray import tune
+from ray.rllib.agents.registry import get_agent_class
+from ray.rllib.agents.ppo.ppo_policy_graph import PPOPolicyGraph
+from ray.rllib.models import ModelCatalog
+from ray.tune import run_experiments
+from ray.tune.registry import register_env
+import tensorflow as tf
+
+from social_dilemmas.envs.harvest import HarvestEnv
+from social_dilemmas.envs.cleanup import CleanupEnv
+from models.conv_to_fc_net import ConvToFCNet
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string(
+    'env', 'cleanup',
+    'Name of the environment to rollout. Can be cleanup or harvest.')
+tf.app.flags.DEFINE_integer(
+    'num_agents', 5,
+    'Number of agent policies')
+tf.app.flags.DEFINE_integer(
+    'num_cpus', 2,
+    'Number of available CPUs')
+tf.app.flags.DEFINE_integer(
+    'num_gpus', 1,
+    'Number of available GPUs')
+tf.app.flags.DEFINE_boolean(
+    'use_gpus_for_workers', False,
+    'Set to true to run workers on GPUs rather than CPUs')
+tf.app.flags.DEFINE_boolean(
+    'use_gpu_for_driver', False,
+    'Set to true to run driver on GPU rather than CPU.')
+tf.app.flags.DEFINE_float(
+    'num_workers_per_device', 2,
+    'Number of workers to place on a single device (CPU or GPU)')
+
+harvest_default_params = {
+    'lr_init': 0.00136,
+    'lr_final': 0.000028,
+    'entropy_coeff': -.000687}
+
+cleanup_default_params = {
+    'lr_init': 0.00126,
+    'lr_final': 0.000012,
+    'entropy_coeff': -.00176}
+
+
+def setup(env, hparams, num_cpus, num_gpus, num_agents, use_gpus_for_workers=False,
+          use_gpu_for_driver=False, num_workers_per_device=1):
+
+    if env == 'harvest':
+        def env_creator(_):
+            return HarvestEnv(num_agents=num_agents)
+        single_env = HarvestEnv()
+    else:
+        def env_creator(_):
+            return CleanupEnv(num_agents=num_agents)
+        single_env = CleanupEnv()
+
+    env_name = env + "_env"
+    register_env(env_name, env_creator)
+
+    obs_space = single_env.observation_space
+    act_space = single_env.action_space
+
+    # Each policy can have a different configuration (including custom model)
+    def gen_policy():
+        return (PPOPolicyGraph, obs_space, act_space, {})
+
+    # Setup PPO with an ensemble of `num_policies` different policy graphs
+    policy_graphs = {}
+    for i in range(num_agents):
+        policy_graphs['agent-' + str(i)] = gen_policy()
+
+    def policy_mapping_fn(agent_id):
+        return agent_id
+
+    # register the custom model
+    model_name = "conv_to_fc_net"
+    ModelCatalog.register_custom_model(model_name, ConvToFCNet)
+
+    algorithm = 'DQN'
+    agent_cls = get_agent_class(algorithm)
+    config = agent_cls._default_config.copy()
+
+    # information for replay
+    config['env_config']['func_create'] = tune.function(env_creator)
+    config['env_config']['env_name'] = env_name
+    config['env_config']['run'] = algorithm
+
+    # Calculate device configurations
+    gpus_for_driver = int(use_gpu_for_driver)
+    cpus_for_driver = 1 - gpus_for_driver
+    if use_gpus_for_workers:
+        spare_gpus = (num_gpus - gpus_for_driver)
+        num_workers = int(spare_gpus * num_workers_per_device)
+        num_gpus_per_worker = spare_gpus / num_workers
+        num_cpus_per_worker = 0
+    else:
+        spare_cpus = (num_cpus - cpus_for_driver)
+        num_workers = int(spare_cpus * num_workers_per_device)
+        num_gpus_per_worker = 0
+        num_cpus_per_worker = spare_cpus / num_workers
+
+    # hyperparams
+    config.update({
+                "train_batch_size": 128,
+                "horizon": 1000,
+                "num_workers": num_workers,
+                "num_gpus": gpus_for_driver,  # The number of GPUs for the driver
+                "num_cpus_for_driver": cpus_for_driver,
+                "num_gpus_per_worker": num_gpus_per_worker,   # Can be a fraction
+                "num_cpus_per_worker": num_cpus_per_worker,   # Can be a fraction
+                "entropy_coeff": hparams['entropy_coeff'],
+                "multiagent": {
+                    "policy_graphs": policy_graphs,
+                    "policy_mapping_fn": tune.function(policy_mapping_fn),
+                },
+                "model": {"custom_model": "conv_to_fc_net", "use_lstm": True,
+                          "lstm_cell_size": 128}
+
+    })
+    return algorithm, env_name, config
+
+
+def main(unused_argv):
+    ray.init(num_cpus=FLAGS.num_cpus, redirect_output=True)
+    if FLAGS.env == 'harvest':
+        hparams = harvest_default_params
+    else:
+        hparams = cleanup_default_params
+    alg_run, env_name, config = setup(FLAGS.env, hparams, FLAGS.num_cpus,
+                                      FLAGS.num_gpus, FLAGS.num_agents,
+                                      FLAGS.use_gpus_for_workers,
+                                      FLAGS.use_gpu_for_driver,
+                                      FLAGS.num_workers_per_device)
+
+    run_experiments({
+        "harvest_test": {
+            "run": alg_run,
+            "env": env_name,
+            "stop": {
+                "training_iteration": 300000
+            },
+            'checkpoint_freq': 1000,
+            "config": config,
+        }
+    })
+
+
+if __name__ == '__main__':
+    tf.app.run(main)
