d516e1d9655a09d922dd804c6048ec5d29ebfecf
Natasha Jaques <jaquesn@mit.edu>
2019-03-25 17:33:20 -0400

    add visibility hparams to moa run script'

--- run_scripts/train_a3c_moa.py
+++ run_scripts/train_a3c_moa.py
@@ -139,8 +139,10 @@ def setup(env, hparams, num_cpus, num_gpus, num_agents, use_gpus_for_workers=Fal
             },
             "model": {"custom_model": "conv_to_fc_net_actions", "use_lstm": True,
                       "lstm_cell_size": 128, "lstm_use_prev_action_reward": True,
-                      "custom_options": {"num_other_agents": num_agents,
-                                         "moa_weight": tune.grid_search[5.0, 10.0, 20.0]}}
+                      "custom_options": {
+                          "num_other_agents": num_agents,
+                          "moa_weight": tune.grid_search[5.0, 10.0, 20.0],
+                          "train_moa_only_when_visible": tune.grid_search[True, False]}}
 
         })
     else:
@@ -162,7 +164,8 @@ def setup(env, hparams, num_cpus, num_gpus, num_agents, use_gpus_for_workers=Fal
             "model": {"custom_model": "conv_to_fc_net_actions", "use_lstm": True,
                       "lstm_cell_size": 128, "lstm_use_prev_action_reward": True,
                       "custom_options": {"num_other_agents": num_agents,
-                                         "moa_weight": 12.0}}
+                                         "moa_weight": 12.0,
+                                         "train_moa_only_when_visible": True}}
 
         })
     return algorithm, env_name, config
