0a1b7b801d9617138fc0c27507d3a99af6b2e67b
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-10-21 11:07:36 -0700

    Add grid search to run script

--- run_scripts/run_cluster_exps.sh
+++ run_scripts/run_cluster_exps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ray exec ray_autoscale.yaml "python train_moa.py --train_batch_size 26000 --training_iterations 1000 --num_cpus 14 \
---exp_name test_moa --use_s3 --num_agents 5 --exp_name reproduce_test" --cluster-name=hparam_sweep --tmux --start --stop
+ray exec ray_autoscale.yaml "python sequential_social_dilemma_games/run_scripts/train_moa.py --train_batch_size 26000 --training_iterations 1000 --num_cpus 14 \
+--exp_name test_moa --use_s3 --num_agents 5 --exp_name reproduce_test --grid_search" --cluster-name=hparam_sweep --start --stop
