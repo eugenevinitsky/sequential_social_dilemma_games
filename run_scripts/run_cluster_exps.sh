aaecb48e7c9b6519a29245f2fcf7790f3225c7fb
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-10-23 17:20:42 -0700

    Remove gputil which errors out when there are no GPUs

--- run_scripts/run_cluster_exps.sh
+++ run_scripts/run_cluster_exps.sh
@@ -3,7 +3,7 @@
 # 10/21 experiments
 
 ray exec ray_autoscale.yaml "python sequential_social_dilemma_games/run_scripts/train_moa.py --train_batch_size 26000 --training_iterations 1000 --num_cpus 30 \
---exp_name moa_harvest --use_s3 --num_agents 5 --exp_name reproduce_test --env harvest --num_gpus 0" --tmux --cluster-name=hparam_sweep --start --stop
+--exp_name moa_harvest --use_s3 --num_agents 5 --exp_name reproduce_test --env harvest" --tmux --cluster-name=hparam_sweep --start --stop
 
 #ray exec ray_autoscale.yaml "python sequential_social_dilemma_games/run_scripts/train_moa.py --train_batch_size 26000 --training_iterations 1000 --num_cpus 14 \
 #--exp_name moa_harvest --use_s3 --num_agents 5 --exp_name reproduce_test --grid_search --env harvest --multi_node" --tmux --cluster-name=hparam_sweep --start --stop
