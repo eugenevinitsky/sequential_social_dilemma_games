8ead19310863cf7f84020a39ade5c01ac0f7aa9d
internetcoffeephone <hugoheemskerk@gmail.com>
2019-11-01 18:32:14 +0100

    Fixed run_curiosity_switch run script, stop_at_timesteps_total was 10x too large.

--- run_scripts/run_curiosity_switch.sh
+++ run_scripts/run_curiosity_switch.sh
@@ -6,7 +6,7 @@ python train_curiosity.py \
 --algorithm A3C \
 --sample_batch_size 1000 \
 --train_batch_size 30000 \
---stop_at_timesteps_total $((50 * 10 ** 6)) \
+--stop_at_timesteps_total $((500 * 10 ** 6)) \
 --memory $((50 * 10 ** 9)) \
 --num_cpus 24 \
 --num_gpus 4 \
