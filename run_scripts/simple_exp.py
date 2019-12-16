8c48d93b55c3484980e5ae14adb43dedb32089a2
internetcoffeephone <hugoheemskerk@gmail.com>
2019-12-16 19:09:58 +0100

    Added simple experiment runfile, used for local debugging.

---
+++ run_scripts/simple_exp.py
@@ -0,0 +1,34 @@
+import ray
+from ray import tune
+from social_dilemmas.envs.env_creator import get_env_creator
+from ray.tune.registry import register_env
+
+
+class Args(object):
+    pass
+
+
+args = Args()
+args.num_switches = 1
+args.env = "harvest"
+
+env_creator = get_env_creator(args.env, 1, args)
+env_name = args.env + "_env"
+register_env(env_name, env_creator)
+
+ray.init()
+tune.run(
+    "PPO",
+    stop={"episode_reward_mean": 200},
+    config={
+        "env": env_name,
+        "horizon": 100,
+        "sample_batch_size": 100,
+        "num_gpus": 1,
+        "num_workers": 7,
+        "num_envs_per_worker": 16,
+        "lr": 0.01,
+        "eager": False,
+        "memory": 800000000,
+    },
+)
