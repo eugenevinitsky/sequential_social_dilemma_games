2166a1401b45886e71a16c21309d187597f3bc70
internetcoffeephone <hugoheemskerk@gmail.com>
2020-01-02 12:20:24 +0100

    Updated run_baseline_cleanup.sh script to fix batch size/gpu issues.

--- run_scripts/run_baseline_cleanup.sh
+++ run_scripts/run_baseline_cleanup.sh
@@ -6,16 +6,16 @@ python train.py \
 --model baseline \
 --algorithm PPO \
 --num_agents 5 \
---sample_batch_size 100 \
---train_batch_size 19200 \
+--sample_batch_size 1000 \
+--train_batch_size 12000 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
 --memory $((50 * 10 ** 9)) \
 --num_cpus 24 \
 --num_gpus 4 \
 --use_gpu_for_driver \
 --use_gpus_for_workers \
---num_workers_per_device 0.125 \
 --num_samples 1 \
+--num_envs_per_worker 4 \
 --lr_curriculum_steps 0 20000000 \
 --lr_curriculum_weights 0.00126 0.000012 \
 --entropy_coeff 0.00176
