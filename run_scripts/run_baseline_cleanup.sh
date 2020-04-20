59095c51871e8b9f7d70835db0c1481224400c7f
internetcoffeephone <hugoheemskerk@gmail.com>
2020-04-20 12:31:53 +0200

    Set final baseline cleanup experiment parameters.

--- run_scripts/run_baseline_cleanup.sh
+++ run_scripts/run_baseline_cleanup.sh
@@ -7,12 +7,12 @@ python train.py \
 --algorithm PPO \
 --num_agents 5 \
 --rollout_fragment_length 1000 \
---train_batch_size 192000 \
+--train_batch_size 128000 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
---memory $((100 * 10 ** 9)) \
---num_workers 24 \
+--memory $((25 * 10 ** 9)) \
+--num_workers 16 \
 --cpus_per_worker 1 \
---gpus_per_worker 0.125 \
+--gpus_per_worker 0 \
 --gpus_for_driver 1 \
 --cpus_for_driver 0 \
 --num_samples 1 \
@@ -20,4 +20,4 @@ python train.py \
 --lr_schedule_steps 0 20000000 \
 --lr_schedule_weights 0.00126 0.000012 \
 --entropy_coeff 0.00176 \
---ppo_sgd_minibatch_size 24000
+--ppo_sgd_minibatch_size 16000
