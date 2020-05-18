0b1a9059850b9c1bf1159f5331d49f0162bad82e
internetcoffeephone <hugoheemskerk@gmail.com>
2020-05-18 17:41:25 +0200

    Escape spaces in ray patch script.

--- ray_uint8_patch.sh
+++ ray_uint8_patch.sh
@@ -8,4 +8,4 @@ sed -i '231s/np.zeros(self.shape)/np.zeros(self.shape, dtype=self.observation_sp
 sed -i '215s/tf.int64/action_space.dtype/' venv/lib/"$python_folder_name"/site-packages/ray/rllib/models/catalog.py # Change action shape to what we actually provide
 sed -i '55s/tf.math.argmax(self.inputs, axis=1)/tf.math.argmax(self.inputs, axis=1, output_type=tf.int32)/' venv/lib/"$python_folder_name"/site-packages/ray/rllib/models/tf/tf_action_dist.py # Actions should not sample at int64, int32 is the lowest that multinomial takes
 sed -i '83s/tf.multinomial(self.inputs, 1)/tf.multinomial(self.inputs, 1, output_dtype=tf.int32)/' venv/lib/"$python_folder_name"/site-packages/ray/rllib/models/tf/tf_action_dist.py # Same as above
-sed -i '644i        actions = np.array(actions, dtype=policy.action_space.dtype)' venv/lib/"$python_folder_name"/site-packages/ray/rllib/evaluation/sampler.py # Insert action to uint8 conversion to save even more memory
+sed -i '644i\        actions = np.array(actions, dtype=policy.action_space.dtype)' venv/lib/"$python_folder_name"/site-packages/ray/rllib/evaluation/sampler.py # Insert action to uint8 conversion to save even more memory
