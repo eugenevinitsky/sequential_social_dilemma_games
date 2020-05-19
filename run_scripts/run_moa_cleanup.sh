c94787a64c40c14d55eb4d4d947fb47e1822692b
internetcoffeephone <hugoheemskerk@gmail.com>
2020-05-19 15:20:11 +0200

    Modify run scripts to use rollout_fragment_length=64 and num_envs_per_worker=64. Remove train_batch_size and ppo_sgd_minibatch_size from run scripts, as these parameters are now automatically calculated when not specified in the args.

--- run_scripts/run_moa_cleanup.sh
+++ run_scripts/run_moa_cleanup.sh
@@ -5,22 +5,20 @@ python train.py \
 --model moa \
 --algorithm PPO \
 --num_agents 5 \
---rollout_fragment_length 1000 \
---train_batch_size 128000 \
+--num_workers 1 \
+--rollout_fragment_length 64 \
+--num_envs_per_worker 64 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
 --memory $((25 * 10 ** 9)) \
---num_workers 16 \
 --cpus_per_worker 1 \
 --gpus_per_worker 0 \
 --gpus_for_driver 1 \
 --cpus_for_driver 0 \
 --num_samples 1 \
---num_envs_per_worker 8 \
 --lr_schedule_steps 0 100000000 \
 --lr_schedule_weights 0.00120 0.000044 \
 --entropy_coeff 0.00176 \
 --moa_loss_weight 15.007 \
 --influence_reward_weight 0.620 \
 --influence_reward_schedule_steps 0 100000000 \
---influence_reward_schedule_weights 0.0 1.0 \
---ppo_sgd_minibatch_size 32000
+--influence_reward_schedule_weights 0.0 1.0
--- run_scripts/run_scm_harvest.sh
+++ run_scripts/run_scm_harvest.sh
@@ -5,17 +5,16 @@ python train.py \
 --model scm \
 --algorithm PPO \
 --num_agents 5 \
---rollout_fragment_length 1000 \
---train_batch_size 128000 \
+--num_workers 1 \
+--rollout_fragment_length 64 \
+--num_envs_per_worker 64 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
 --memory $((25 * 10 ** 9)) \
---num_workers 16 \
 --cpus_per_worker 1 \
 --gpus_per_worker 0 \
 --gpus_for_driver 1 \
 --cpus_for_driver 0 \
 --num_samples 1 \
---num_envs_per_worker 8 \
 --lr_schedule_steps 0 100000000 \
 --lr_schedule_weights 0.00120 0.000044 \
 --entropy_coeff 0.00223 \
@@ -25,5 +24,4 @@ python train.py \
 --influence_reward_schedule_weights 0.0 1.0 \
 --scm_loss_weight 1.0 \
 --scm_forward_vs_inverse_loss_weight 0.5 \
---curiosity_reward_weight 0.001 \
---ppo_sgd_minibatch_size 32000
+--curiosity_reward_weight 0.001
