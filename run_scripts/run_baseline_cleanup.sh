e7eaeabaa057c7f050e71aef29fdcd2ef77032fb
internetcoffeephone <hugoheemskerk@gmail.com>
2020-08-14 17:49:36 +0200

    Update run_baseline_* scripts to reflect final experiment settings.

--- run_scripts/run_baseline_cleanup.sh
+++ run_scripts/run_baseline_cleanup.sh
@@ -5,15 +5,16 @@ python train.py \
 --model baseline \
 --algorithm PPO \
 --num_agents 5 \
---num_workers 2 \
---rollout_fragment_length 64 \
---num_envs_per_worker 32 \
+--num_workers 6 \
+--rollout_fragment_length 1000 \
+--num_envs_per_worker 16 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
 --memory $((160 * 10 ** 9)) \
 --cpus_per_worker 1 \
 --gpus_per_worker 0 \
---gpus_for_driver 0.333 \
+--gpus_for_driver 1 \
 --cpus_for_driver 0 \
 --num_samples 5 \
 --entropy_coeff 0.00176 \
---tune_hparams
+--lr_schedule_steps 0 20000000 \
+--lr_schedule_weights .00126 .000012
